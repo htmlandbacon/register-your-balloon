cee26ee6a4b138c6e125f7919ee5180b55221021
Colin Oakley <colin.oakley@engineering.digital.dwp.gov.uk>
2021-07-21 16:26:42 +0100

    adding all the pages

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,31 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/steps/about-you', (req, res) => {
+    res.redirect('/steps/address');
+});
+
+router.post('/steps/address', (req, res) => {
+    res.redirect('/steps/contact');
+});
+
+router.post('/steps/contact', (req, res) => {
+    res.redirect('/steps/balloon-details');
+});
+
+router.post('/steps/balloon-details', (req, res) => {
+    res.redirect('/steps/balloon-more');
+});
+
+router.post('/steps/balloon-more', (req, res) => {
+    res.redirect('/steps/check-your-answers');
+});
+
+router.post('/steps/check-your-answers', (req, res) => {
+    res.redirect('/steps/complete');
+});
+
+
+
+
 module.exports = router
